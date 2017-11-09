1aa5ee00c65519cc02e3dcbd353adb7b5cd12cb5
Philip Diffenderfer <pdiffenderfer@gmail.com>
2017-11-08 21:58:35 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+![anim8js](https://github.com/anim8js/anim8js/blob/master/images/anim8js-logo.png)
+
+[anim8](https://github.com/anim8js/anim8js) your [ScrollMagic](http://scrollmagic.io/) project.
+
+### Installation
+
+- Bower: `bower install anim8js-scrollmagic`
+- Node: `npm install anim8js-scrollmagic`
+- Download: [anim8js-scrollmagic](https://raw.githubusercontent.com/anim8js/anim8js-scrollmagic/master/build/anim8js-scrollmagic.js)
---
+++ src/_header.js
@@ -0,0 +1,31 @@
+// UMD (Universal Module Definition)
+(function (root, factory)
+{
+  if (typeof define === 'function' && define.amd) // jshint ignore:line
+  {
+    // AMD. Register as an anonymous module.
+    define(['anim8', 'scrollmagic'], function(anim8, ScrollMagic) { // jshint ignore:line
+      return factory(anim8, ScrollMagic, root);
+    });
+  }
+  else if (typeof module === 'object' && module.exports)  // jshint ignore:line
+  {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require('anim8'), require('scrollmagic'), global);  // jshint ignore:line
+  }
+  else
+  {
+    // Browser globals (root is window)
+    factory(root.anim8, root.ScrollMagic, root);
+  }
+}(this, function(anim8, ScrollMagic, window)
+{
+
+  var anim8s = anim8.anim8s;
+  var Class = anim8.Class;
+
+  var $animation = anim8.animation;
+
+  var Scene = ScrollMagic.Scene.prototype;
